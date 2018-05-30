af212fe44e1e7ef5f4af12890f3d93f13fc8db91
Vincent Pietri <vincent.pietri@agence-tbd.com>
2018-05-30 17:04:41 +0200

    Shortcut: fix translation

--- app/code/community/Monext/Payline/Model/Datasource/Checkout/Method.php
+++ app/code/community/Monext/Payline/Model/Datasource/Checkout/Method.php
@@ -4,8 +4,8 @@ class Monext_Payline_Model_Datasource_Checkout_Method
     public function toOptionArray()
     {
         $options = array();
-        $options[] = array('value' => Mage_Checkout_Model_Type_Onepage::METHOD_GUEST, 'label' => 'Guest');
-        $options[] = array('value' => Mage_Checkout_Model_Type_Onepage::METHOD_REGISTER, 'label' => 'Register and auto log');
+        $options[] = array('value' => Mage_Checkout_Model_Type_Onepage::METHOD_GUEST, 'label' => Mage::helper('payline')->__('Guest'));
+        $options[] = array('value' => Mage_Checkout_Model_Type_Onepage::METHOD_REGISTER, 'label' => Mage::helper('payline')->__('Register and auto log'));
 
         return $options;
     }
